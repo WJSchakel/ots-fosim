d5eb6e0bae3a423ea9461c57721504d35d1ecabe
wjschakel <wjschakel@TUD264483.tudelft.net>
2024-11-13 12:08:25 +0100

    Included package-info.java's.

---
+++ ots-fosim/src/main/java/org/opentrafficsim/fosim/model/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * FOSIM-specific model components for OTS.
+ * <p>
+ * Copyright (c) 2023-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
+ */
+package org.opentrafficsim.fosim.model;
---
+++ ots-fosim/src/main/java/org/opentrafficsim/fosim/parameters/data/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Definition of parameter data as sent from FOSIM to OTS. This is similar to the super package, but the definitions in this
+ * package are simpler as the parameters do not contain meta data, but only the values or distributions.
+ * <p>
+ * Copyright (c) 2023-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="https://opentrafficsim.org/docs/license.html">OpenTrafficSim License</a>.
+ * </p>
+ * @author <a href="https://github.com/wjschakel">Wouter Schakel</a>
+ */
+package org.opentrafficsim.fosim.parameters.data;
